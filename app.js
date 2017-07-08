31de2efbc0a4eccce5ec41ec117b38abfb856fb4
Omar Cameron <omarjcameron@gmail.com>
2017-07-08 15:27:52 -0700

    Add API keys

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const keyPublishable = process.env.PUBLISHABLE_KEY;
-const keySecret = process.env.SECRET_KEY;
+const keyPublishable = process.env.pk_test_9lljIg1Rl0nShh2dcly87dp8;
+const keySecret = process.env.sk_test_FmhL1y6U0mumewpJe1I4cFLy;
 
 const app = require("express")();
 const stripe = require("stripe")(keySecret);
